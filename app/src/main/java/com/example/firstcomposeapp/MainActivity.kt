82953c40d825edc3b45a97b36f1a28b176a249bf
Murad <m.iusupov>
2022-11-27 14:14:03 +0300

    learning states

--- app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapp/MainActivity.kt
@@ -1,93 +1,66 @@
 package com.example.firstcomposeapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
-                ListItem("Freedom", "Ichkeria")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
-
         }
     }
 }
 
 @Composable
-private fun ListItem(name: String, country: String) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp)
-            .pointerInput(Unit) {
-                detectHorizontalDragGestures { change, dragAmount ->
-                    Log.d("MyTag", "Long press $dragAmount")
-                }
-            },
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box() {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.flag),
-                    contentDescription = "image",
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .size(64.dp)
-                        .clip(CircleShape)
-                )
-                Column(
-                    modifier = Modifier.padding(start = 16.dp)
-                ) {
-                    Text(text = name)
-                    Text(text = country)
-                }
-            }
-        }
+private fun CircleItem() {
+    val counter = remember {
+        mutableStateOf(0)
     }
+
+    val color = remember {
+        mutableStateOf(Color.Blue)
+    }
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .background(color = color.value, shape = CircleShape)
+            .clickable {
+                when(++counter.value) {
+                    10 -> color.value = Color.Red
+                    20 -> color.value = Color.Green
+                }
+
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = counter.value.toString(),
+            style = TextStyle(color = Color.White, fontSize = 25.sp)
+        )
+    }
+
 }
